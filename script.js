06c896d31a784c99d914634f0aa125222f3e1c5f
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2024-04-05 10:14:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',()=>{
+	const submitBtn = document.getElementById('submit');
+	const existingBtn = document.getElementById('existing');
+
+	submitBtn.addEventListener('click',(e)=>{
+		e.preventDefault();
+	let username = document.getElementById('username').value;
+	let password = document.getElementById('password').value;
+	let isCheck = document.getElementById('check').checked;
+		if(isCheck){
+			let loginDetails = {username,password};
+			localStorage.setItem('loginDetails',JSON.stringify(loginDetails));
+		}else{
+			localStorage.removeItem('loginDetails');
+		}
+		alert("Logged in as " + username);
+	});
+
+	let saveDetails = JSON.parse(localStorage.getItem('loginDetails'));
+	if(saveDetails){
+		existingBtn.style.display = "block";
+		existingBtn.addEventListener('click',()=>{
+			alert("Logged in as " + saveDetails.username);
+		});
+	}else{
+		existingBtn.style.display = "none";
+	}
+});
+	
+	 
+	
